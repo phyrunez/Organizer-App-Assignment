a60f31c3b57d46a21f9365d8f5d5140454ac7c96
Jude <jude.eze@smartflowtech.com>
2023-05-18 08:50:34 -0400

    passing down props data properly and passing data up from child using events

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
     <div id="app">
       <navbar />
-        <router-view></router-view>
+      <router-view></router-view>
     </div> 
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
 import Tasks from './components/Tasks/Tasks.vue'
-// import Form from './components/Tasks/Form.vue'
 import Home from './components/Home.vue'
 
 export default {
